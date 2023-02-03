129ad140a13e81e5c51894a3cd46ce24b44e0f9f
Davor Stajcer <Davor Stajcer>
2023-02-03 08:42:13 +0100

    Add dnevnik publisher call string

---
+++ app/src/main/java/com/example/bannerintegration/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.bannerintegration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.admanager.AdManagerAdRequest;
+import com.sourcepoint.cmplibrary.model.MessageLanguage;
+import com.yieldlove.adIntegration.AdFormats.YieldloveBannerAd;
+import com.yieldlove.adIntegration.AdFormats.YieldloveBannerAdListener;
+import com.yieldlove.adIntegration.AdFormats.YieldloveBannerAdView;
+import com.yieldlove.adIntegration.Yieldlove;
+import com.yieldlove.adIntegration.YieldloveConsent;
+import com.yieldlove.adIntegration.exceptions.YieldloveException;
+
+public class MainActivity extends AppCompatActivity {
+
+    YieldloveConsent consent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ViewGroup adContainer = findViewById(R.id.AdContainer);
+
+        Yieldlove.setApplicationName("yl_dnevnik");
+
+        this.consent = new YieldloveConsent(
+                this,
+                R.id.main);
+
+        this.consent.collect(MessageLanguage.ENGLISH);
+
+        try {
+            YieldloveBannerAd ad = new YieldloveBannerAd(this);
+
+            ad.load("/18087324/app_1", new YieldloveBannerAdListener() {
+
+                @Override
+                public AdManagerAdRequest.Builder onAdRequestBuild() {
+                    return null;
+                }
+
+                @Override
+                public void onAdLoaded(YieldloveBannerAdView banner) {
+                    adContainer.addView(banner.getAdView());
+                }
+
+                @Override
+                public void onAdFailedToLoad(YieldloveBannerAdView yieldloveBannerAdView, YieldloveException e) {
+                    Toast.makeText(getApplicationContext(), "Ad load failed", Toast.LENGTH_LONG).show();
+                    Log.e("yieldstuff:",e.getMessage());
+                }
+
+                @Override
+                public void onAdOpened(YieldloveBannerAdView yieldloveBannerAdView) {
+                    Toast.makeText(getApplicationContext(), "Ad opened", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onAdClosed(YieldloveBannerAdView yieldloveBannerAdView) {
+                    Toast.makeText(getApplicationContext(), "Ad closed", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onAdClicked(YieldloveBannerAdView yieldloveBannerAdView) {
+                    Toast.makeText(getApplicationContext(), "Ad clicked", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onAdImpression(YieldloveBannerAdView yieldloveBannerAdView) {
+                    Toast.makeText(getApplicationContext(), "Ad impression", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        } catch (YieldloveException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void btnPrivacyClick(View view) {
+        this.consent.showPrivacyManager(MessageLanguage.ENGLISH);
+    }
+}
